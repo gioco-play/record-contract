cc81f6c8fa06b4ccd6d1fd656cd893e528875891
Ellie Su <ellie.su@goldenf.co>
2022-03-08 14:47:50 +0800

    update operator type

--- src/Contract/RecordServiceInterface.php
+++ src/Contract/RecordServiceInterface.php
@@ -9,35 +9,35 @@ interface RecordServiceInterface {
     /**
      * 大表投注紀錄
      *
-     * @param string $operatorCode 營商代碼
+     * @param mixed $operatorCode 營商代碼
      * @param array $requireInputs 必填欄位
      * @return array
      */
-    function betlogs(string $operatorCode, array $requireInputs): array;
+    function betlogs($operatorCode, array $requireInputs): array;
 
     /**
      * 大表投注紀錄(寫入postgres)
      *
-     * @param string $operatorCode
+     * @param mixed $operatorCode
      * @param array $requireInputs
      * @return array
      */
-    function betlogsToPostgres(string $operatorCode, array $requireInputs): array;
+    function betlogsToPostgres($operatorCode, array $requireInputs): array;
 
     /**
      * 大表投注紀錄(多筆寫入postgres)
      *
-     * @param string $operatorCode
+     * @param mixed $operatorCode
      * @param string $vendorCode
      * @param array $betlogs
      * @return array
      */
-    function betlogsToPostgresMultiple(string $operatorCode, string $vendorCode, array $betlogs): array;
+    function betlogsToPostgresMultiple($operatorCode, string $vendorCode, array $betlogs): array;
 
     /**
      * 小表投注紀錄
      *
-     * @param string $operatorCode 營商代碼
+     * @param mixed $operatorCode 營商代碼
      * @param string $vendorCode 遊戲商代碼
      * @param array $requireInputs 必填欄位
      * @param array $extraInputs 額外欄位
@@ -45,63 +45,63 @@ interface RecordServiceInterface {
      * @param bool $needCreatedAt 產生建立時間
      * @return array
      */
-    function betlog(string $operatorCode, string $vendorCode, array $requireInputs, array $extraInputs, array $rawData, bool $needCreatedAt = true): array;
+    function betlog($operatorCode, string $vendorCode, array $requireInputs, array $extraInputs, array $rawData, bool $needCreatedAt = true): array;
 
     /**
      * 小表投注紀錄（多筆）
      *
-     * @param string $operatorCode
+     * @param mixed $operatorCode
      * @param string $vendorCode
      * @param array $betlogs
      * @return array
      */
-    function betlogMultiple(string $operatorCode, string $vendorCode, array $betlogs, bool $needCreatedAt = true): array;
+    function betlogMultiple($operatorCode, string $vendorCode, array $betlogs, bool $needCreatedAt = true): array;
 
 
     /**
      * 更新小表的createdat
      *
-     * @param string $operatorCode
+     * @param mixed $operatorCode
      * @param string $vendorCode
      * @param array $betId
      * @return array
      */
-    function betlogUpdateCreateAt(string $operatorCode, string $vendorCode, array $betId): array;
+    function betlogUpdateCreateAt($operatorCode, string $vendorCode, array $betId): array;
 
     /**
      * 查詢小表注單
      *
-     * @param string $operatorCode
+     * @param mixed $operatorCode
      * @param string $vendorCode
      * @param string $betId
      * @return array
      */
-    function queryBetlog(string $operatorCode, string $vendorCode, string $betId, string $searchField = 'bet_id'): array;
+    function queryBetlog($operatorCode, string $vendorCode, string $betId, string $searchField = 'bet_id'): array;
 
 
     /**
      * 儲存疑慮注單
      *
-     * @param string $operatorCode 營商代碼
+     * @param mixed $operatorCode 營商代碼
      * @param string $vendorCode 遊戲商代碼
      * @param array $requireInputs 必填欄位
      * @param array $extraInputs 額外欄位
      * @param array $rawData 原始資料
      * @return array
      */
-    function suspectBetlog(string $operatorCode, string $vendorCode, array $requireInputs, array $extraInputs, array $rawData): array;
+    function suspectBetlog($operatorCode, string $vendorCode, array $requireInputs, array $extraInputs, array $rawData): array;
 
     /**
      * 儲存紅利注單
      *
-     * @param string $operatorCode 營商代碼
+     * @param mixed $operatorCode 營商代碼
      * @param string $vendorCode 遊戲商代碼
      * @param array $requireInputs 必填欄位
      * @param array $extraInputs 額外欄位
      * @param array $rawData 原始資料
      * @return array
      */
-    function gameBonusTransLog(string $operatorCode, string $vendorCode, array $requireInputs, array $extraInputs, array $rawData): array;
+    function gameBonusTransLog($operatorCode, string $vendorCode, array $requireInputs, array $extraInputs, array $rawData): array;
 
 }
 
