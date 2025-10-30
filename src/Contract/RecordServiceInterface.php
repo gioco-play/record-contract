31c83b3745571c3547ef1d8ba8ee008e0efdfc4b
Amin56413 <ellie.guo@goldenf.co>
2025-10-30 14:43:30 +0800

    update gameBonusTransLogV2  comment

--- src/Contract/RecordServiceInterface.php
+++ src/Contract/RecordServiceInterface.php
@@ -114,7 +114,13 @@ interface RecordServiceInterface {
      * @param int $transactionTime  必填 - 13 位數，unixTimestampMs
      * @param string $traceId       必填 - unique id
      * @param string $eventId       必填 - 事件 id, 請勿填入空值
-     * @param array $extraInputs    選填 - game_code, bet_id, parent_bet_id, memo, jp_contribution(彩池貢獻金), jp_type(彩池類型)
+     * @param array $extraInputs    選填
+     * - game_code
+     * - bet_id (需和parent_bet_id 成對出現)
+     * - parent_bet_id (需和bet_id 成對出現)
+     * - memo
+     * - jp_contribution(彩池貢獻金)
+     * - jp_type(彩池類型)
      * @param array $rawData        必填 - 原始資料
      * @return array
      */
